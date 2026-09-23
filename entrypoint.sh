cc22ad289bcc934bfbc01f4f151a3fe9b6f797f7
Zoie Firkins <134826281+zmfirkins@users.noreply.github.com>
2026-09-23 17:47:31 +0000

    agh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-set -e
 
-echo "========"
+echo "==================="
 
-git config --global user.name "${INPUT_NAME:-${GITHUB_ACTOR}}"
+git config --global user.name "${GITHUB_ACTOR}"
 git config --global user.email "${INPUT_EMAIL}"
 git config --global --add safe.directory /github/workspace
 
 python3 /usr/bin/feed.py
 
 git add -A && git commit -m "Update Feed"
-
 git push --set-upstream origin main
 
-
-
-echo "========"
+echo "==================="
